a003f5ba7b305f293cc62d32424a55a2f68d94fd
lesprgm <lesozzy5@gmail.com>
2025-01-28 21:43:20 -0500

    Added packages and facial recognition script

--- main.py
+++ main.py
@@ -7,3 +7,17 @@ import math
 def face_confidence(face_distance, face_match_threshold=0.6):
     range = (1.0 - face_match_threshold)
     linear_val = (1.0 - face_distance) / (range * 2.0)
+    
+    if face_distance > face_match_threshold:
+        return str(round(linear_val * 100, 2)) + '%'
+    else:
+        value = (linear_val +((1.0 - linear_val)* math.pow((linear_val - 0.5) * 2, 0.2))) * 100
+        return str(round(value, 2)) + '%'
+ 
+class FaceRecogniton:
+    face_locations = []
+    face_encodings = []
+    face_names = []
+    known_face_encodings =[]
+
+    
