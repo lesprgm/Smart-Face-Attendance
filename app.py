b1525a344d5b89d3fc401b3ed51d1aa438eba4fe
lesprgm <lesozzy5@gmail.com>
2025-05-04 17:05:12 -0400

    Added files for deployment

--- app.py
+++ app.py
@@ -214,5 +214,6 @@ def attendance():
 def serve_face(filename):
     return send_from_directory('faces', filename)
 
-if __name__ == '__main__':
-    app.run(debug=True) 
+if __name__ == "__main__":
+    import os
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
